5359c86985ccd71e862f47c55b0ee4c0b7d70432
Adenike <adeyinkaadenike992@gmail.com>
2024-01-25 20:07:02 +0100

    commited

---
+++ app.py
@@ -0,0 +1,183 @@
+# from tkinter import *
+
+# app = Tk()
+
+# app.mainloop()
+
+
+# import math
+
+# app = math
+
+# num = app.sqrt(144)
+
+# print(num)
+
+# print("Hello, world")
+
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+
+# Varuable 
+
+# name = "cow"
+# age = '70'
+
+# print("There once was a man named " +  name + "," )
+# age = '10'
+# print("He was "  + age + "yrs old." )
+
+
+# def my_function(fname):
+#     print("Hello world " + fname)
+    
+    
+# my_function("precious")
+
+# num = 19
+# print(f"Adenike is {num} years old")
+
+
+# number = int(input("Please enter a number between 10 and 50: "))
+# print("You entered:", number)
+# if 10 <= number <= 50:
+    
+    
+#     if number <= 10:
+#         print("I love you")
+#     elif number == 11:
+#         print("I hate you")
+#     elif number == 12:
+#         print("You are my future husband")
+#     elif number == 13:
+#         print("You are my future wife")
+#     elif number == 14:
+#         print("Best friend")
+#     elif number == 15:
+#         print("You are awesome")
+#     elif number == 16:
+#         print("You are unique")
+#     elif number == 17:
+#         print("You are my inspiration")
+#     elif number == 18:
+#         print("You make me smile")
+#     elif number == 19:
+#         print("You brighten my day")
+#     elif number == 20:
+#         print("You are my sunshine")
+#     elif number == 21:
+#         print("You bring joy to my life")
+#     elif number == 22:
+#         print("You are a blessing")
+#     elif number == 23:
+#         print("You are my rock")
+#     elif number == 24:
+#         print("You are my hero")
+#     elif number == 25:
+#         print("You are a star")
+#     # Add more conditions for numbers up to 50 as needed
+#     elif number == 50:
+#         print("You reached 50!")
+#     else:
+#         print("You entered:", number)
+#         print("Other responses for numbers between 15 and 49")
+# else:
+#     print("The number you entered is not between 10 and 50.")
+
+
+
+
+number = int(input("Please enter a number between 10 and 50: "))
+
+if 10 <= number <= 50:
+    print("You entered:", number)
+    
+    if number == 10:
+        print("I love you")
+    elif number == 11:
+        print("I hate you")
+    elif number == 12:
+        print("You are my future husband")
+    elif number == 13:
+        print("You are my future wife")
+    elif number == 14:
+        print("Best friend")
+    elif number == 15:
+        print("You are awesome")
+    elif number == 16:
+        print("You are unique")
+    elif number == 17:
+        print("You are my inspiration")
+    elif number == 18:
+        print("You make me smile")
+    elif number == 19:
+        print("You brighten my day")
+    elif number == 20:
+        print("You are my sunshine")
+    elif number == 21:
+        print("You are outstanding")
+    elif number == 22:
+        print("You are incredible")
+    elif number == 23:
+        print("You are a star")
+    elif number == 24:
+        print("You are my hero")
+    elif number == 25:
+        print("You are my rock")
+    elif number == 26:
+        print("You are my support")
+    elif number == 27:
+        print("You are amazing")
+    elif number == 28:
+        print("You are phenomenal")
+    elif number == 29:
+        print("You are fantastic")
+    elif number == 30:
+        print("You are wonderful")
+    elif number == 31:
+        print("You are remarkable")
+    elif number == 32:
+        print("You are exceptional")
+    elif number == 33:
+        print("You are extraordinary")
+    elif number == 34:
+        print("You are astonishing")
+    elif number == 35:
+        print("You are sensational")
+    elif number == 36:
+        print("You are magnificent")
+    elif number == 37:
+        print("You are splendid")
+    elif number == 38:
+        print("You are glorious")
+    elif number == 39:
+        print("You are outstanding")
+    elif number == 40:
+        print("You are superb")
+    elif number == 41:
+        print("You are stellar")
+    elif number == 42:
+        print("You are fabulous")
+    elif number == 43:
+        print("You are impressive")
+    elif number == 44:
+        print("You are terrific")
+    elif number == 45:
+        print("You are phenomenal")
+    elif number == 46:
+        print("You are majestic")
+    elif number == 47:
+        print("You are grand")
+    elif number == 48:
+        print("You are monumental")
+    elif number == 49:
+        print("You are legendary")
+    elif number == 50:
+        print("You are a fool") 
+    else:
+        print("Invalid number")
+else:
+    print("You entered:", number)
+    print("The number you entered is not between 10 and 50.")
---
+++ snake.py
@@ -0,0 +1,161 @@
+import turtle
+import time
+import random
+
+delay = 0.1
+score = 0
+
+# Set up the screen
+win = turtle.Screen()
+win.title("Snake Game")
+win.bgcolor("black")
+win.setup(width=600, height=600)
+win.tracer(0)  # Turns off the screen updates
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0, 0)
+head.direction = "Right"
+
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0, 100)
+
+# Snake body
+segments = []
+
+# Score display
+score_display = turtle.Turtle()
+score_display.speed(0)
+score_display.color("white")
+score_display.penup()
+score_display.hideturtle()
+score_display.goto(0, 260)
+score_display.write("Score: 0", align="center", font=("Courier", 24, "normal"))
+
+# Functions
+def go_up():
+    if head.direction != "Down":
+        head.direction = "Up"
+
+def go_down():
+    if head.direction != "Up":
+        head.direction = "Down"
+
+def go_left():
+    if head.direction != "Right":
+        head.direction = "Left"
+
+def go_right():
+    if head.direction != "Left":
+        head.direction = "Right"
+
+def move():
+    if head.direction == "Up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "Down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "Left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "Right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# Keyboard bindings
+win.listen()
+win.onkey(go_up, "Up")
+win.onkey(go_down, "Down")
+win.onkey(go_left, "Left")
+win.onkey(go_right, "Right")
+
+# Main game loop
+while True:
+    win.update()
+
+    # Check for a collision with the border
+    if (
+        head.xcor() > 290
+        or head.xcor() < -290
+        or head.ycor() > 290
+        or head.ycor() < -290
+    ):
+        head.goto(0, 0)
+        head.direction = "Stop"
+        time.sleep(1)
+        for segment in segments:
+            segment.goto(1000, 1000)  # Move segments off-screen
+        segments.clear()
+        score = 0
+        delay = 0.1
+        score_display.clear()
+        score_display.write("Score: {}".format(score), align="center", font=("Courier", 24, "normal"))
+
+    # Check for a collision with the food
+    if head.distance(food) < 20:
+        x = random.randint(-270, 270)
+        y = random.randint(-270, 270)
+        food.goto(x, y)
+
+        # Add a new segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        # Increase the score
+        score += 10
+        if score % 50 == 0:
+            delay -= 0.01
+
+        score_display.clear()
+        score_display.write("Score: {}".format(score), align="center", font=("Courier", 24, "normal"))
+
+    # Move the end segments first in reverse order
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x, y)
+
+    # Move the first segment to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+
+    # Check for head collisions with the body
+    for segment in segments:
+        if head.distance(segment) < 20:
+            head.goto(0, 0)
+            head.direction = "Stop"
+            time.sleep(1)
+            for seg in segments:
+                seg.goto(1000, 1000)  # Move segments off-screen
+            segments.clear()
+            score = 0
+            delay = 0.1
+            score_display.clear()
+            score_display.write("Score: {}".format(score), align="center", font=("Courier", 24, "normal"))
+
+    move()
+
+    time.sleep(delay)
+
+# This line is necessary to keep the window open
+win.mainloop()
+
